f946192b71abd2cceb9e190d0ae10fa495a74ddd
Felipe Rezende <feliperezende.barbosa@gmail.com>
2023-09-07 22:12:14 -0300

    Controller and Data created

---
+++ DevSquad.Api.Squads/Data/SquadsDbContext.cs
@@ -0,0 +1,15 @@
+using DevSquad.Api.Squads.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevSquad.Api.Squads.Data
+{
+    public class SquadsDbContext : DbContext
+    {
+        public SquadsDbContext(DbContextOptions<SquadsDbContext> options) : base(options)
+        {            
+        }
+
+        public DbSet<Squad> Squads { get; set; }
+        public DbSet<Developer> Developers { get; set; }
+    }
+}
