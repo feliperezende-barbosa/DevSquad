2b2aa4f481e15e1e52f1b1a11364a9073ce70268
Felipe Rezende <feliperezende.barbosa@gmail.com>
2023-09-08 09:06:53 -0300

    Validiting the controller in postman

--- src/DevSquad.Api.Squads/Controllers/SquadsController.cs
+++ src/DevSquad.Api.Squads/Controllers/SquadsController.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using DevSquad.Api.Squads.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevSquad.Api.Squads.Data
@@ -10,6 +7,48 @@ namespace DevSquad.Api.Squads.Data
     [Route("api/[controller]")]
     public class SquadsController : ControllerBase
     {
+        public List<Squad> GetSquads()
+        {
+            return new List<Squad>
+            {
+                new Squad
+                {
+                    Id = 1,
+                    Name = "Squad 1",
+                    Developers = new List<Developer>
+                    {
+                        new Developer
+                        {
+                            Id = 1,
+                            Name = "Member 1"
+                        },
+                        new Developer
+                        {
+                            Id = 2,
+                            Name = "Member 2"
+                        }
+                    }
+                },
+                new Squad
+                {
+                    Id = 2,
+                    Name = "Squad 2",
+                    Developers = new List<Developer>
+                    {
+                        new Developer
+                        {
+                            Id = 3,
+                            Name = "Member 3"
+                        },
+                        new Developer
+                        {
+                            Id = 4,
+                            Name = "Member 4"
+                        }
+                    }
+                }
+            };
+        }
         
     }
 }
